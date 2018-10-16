5511536931f82ca5c2814b8eddffb4ec302efbe0
Michael Kubicki <Kubicki-Michael@live.de>
2018-10-16 10:14:10 +0200

    Part of first assignment
    Signed-off-by: Michael Kubicki <Kubicki-Michael@live.de>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Machine Learning - Programming Assignments
+
+TODO: Write something
---
+++ assignment-1.rb
@@ -0,0 +1,67 @@
+script = File.basename __FILE__
+
+unless $PROGRAM_NAME == __FILE__
+  puts "#{script} is script not module!"
+  exit(1)
+end
+
+require 'optparse'
+require 'csv'
+
+# commandline parsing
+options = {}
+option_parser = OptionParser.new do |opt|
+  opt.banner = "Usage: #{script} [Arguments]"
+  opt.separator ''
+  opt.on('-h', '--help', 'Display help') do
+    puts opt
+  end
+  opt.separator 'required Arguments:'
+  opt.on('-d', '--data FILE', 'Path to data .csv') do |path|
+    options[:data_path] = path
+  end
+  opt.on('-l', '--learningRate VALUE', 'Learning rate') do |rate|
+    options[:learning_rate] = rate.to_f
+  end
+  opt.on('-t', '--threshold VALUE', 'Threshold value') do |value|
+    options[:threshold] = value.to_f
+  end
+  opt.separator ''
+  opt.separator 'optional Arguments:'
+  opt.on('-o', '--output [FILE]', 'Specify output-file') do |path|
+    options[:output_path] = path
+  end
+end
+
+option_parser.parse!
+
+# Check input
+if options[:data_path].nil? || options[:learning_rate].nil? || options[:threshold].nil?
+  puts "Missing Arguments. See #{script} -h"
+  exit(-1)
+end
+
+unless File.file? options[:data_path]
+  puts "#{options[:data_path]} doesn't exist!"
+  exit(-1)
+end
+
+# preparing output file
+options[:output_path] = File.basename(options[:data_path], '.*') << '_output.csv' if options[:output_path].nil?
+
+# File.delete(options[:output_path]) if File.exist? options[:output_path]
+
+# loading
+puts 'Loading training data'
+training_data = CSV.read(options[:data_path])
+
+# begin-message
+puts "Start training with η=#{options[:learning_rate]} and threshold=#{options[:threshold]}"
+puts "Output will be saved in #{options[:output_path]}"
+
+#outputfile
+output_file = CSV.open(options[:output_path], 'w+')
+
+output_file << %w[Test this shit!]
+
+output_file.close
